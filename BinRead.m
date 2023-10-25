c8aea7641457e12eb4424b3601ac6b0cb9893827
yakboyslim <58483496+yakboyslim@users.noreply.github.com>
2023-10-25 00:22:45 -0400

    V2.0 Big updates

--- BinRead.m
+++ BinRead.m
@@ -2,20 +2,8 @@ function [output] = BinRead(bin,req)
 % [fileopen,pathopen]=uigetfile()
 % bin=fopen(fullfile(pathopen,fileopen));
 
-% req1=[0x24B669 0x24B62A 0x24BB66]
-% req2=[12 1 1]
-% req3=[8 12 8]
-% req4=[128 0 0]
-% req5=[5.12 12.06 1]
-% req6=["uint8" "uint16" "uint16"]
-
 for j=1:length(req{1})
 fseek(bin,req{1}(j),'bof')
 temp=fread(bin,[req{2}(j) req{3}(j)],req{6}(j))
 output{j}=(temp-req{4}(j))./req{5}(j)
 end
-
-% req=[0x24B669 12 8 128 5.12]
-% fseek(bin,req(1),'bof')
-% MAF_STD=fread(bin,[req(2) req(3)])
-% MAF_STD=(MAF_STD-req(4))./req(5)
--- MAF.m
+++ MAF.m
@@ -1,25 +1,29 @@
-function [Res_MAF] = MAF(log,mafxaxis,mafyaxis,current,logvars)
+function [results] = MAF(log,mafxaxis,mafyaxis,maftables,combmodes,logvars)
 % [filestock,pathstock]=uigetfile('*.csv','Select Stock MAF_STD Table CSV File');
 % current=csvread(fullfile(pathstock,filestock))
 
-prompt = {'Maximum confidence:','Minimum count to discard:','Count for max confidence:'};
-dlgtitle = 'Inputs';
+prompt = {'Confidence required to make change:'};
+dlgtitle = 'MAF Correction Inputs';
 dims = [1 50];
-definput = {'0.75','10','100'};
+definput = {'0.95'};
 answer = inputdlg(prompt,dlgtitle,dims,definput)
-fudge=str2num(answer{1});
-mincount=str2num(answer{2});
-maxcount=str2num(answer{3});
+conf=str2num(answer{1});
+maxcount=100
 
 
 %% Create Derived Values
 
 log.MAP=log.MAP.*10
-log.LAM_PCT=100*(log.LAMBDA-log.LAMBDA_SP)./log.LAMBDA_SP
-log.ADD_MAF=log.STFT+log.LTFT+log.MAF_COR
+% log.LAM_PCT=100*(log.LAMBDA-log.LAMBDA_SP)./log.LAMBDA_SP
+% log.LAM_DIF=1./log.LAMBDA_SP-1./log.LAMBDA
+log.ADD_MAF=log.FAC_LAM_COR+log.MAF_COR-log.LAM_DIF
  
 log(log.EngState<2,:) = [];
 log(log.EngState>4,:) = [];
+log(log.state_lam~=1,:) = [];
+log(log.LAMBDA==0,:) = [];
+
+
 %% Read Axis Values
 % stock=csvread(fullfile(getcurrentdir,"VE_stock.csv"))
 % mafxaxis=current(1,2:width(current))
@@ -42,74 +46,90 @@ for i=1:length(mafyaxis)-1;
     yedges(i+1)=(mafyaxis(i)+mafyaxis(i+1))/2;
 end
 
-%% Initialize matrixes
 
-SUM=zeros(length(mafyaxis),length(mafxaxis));
-COUNT=SUM;
-AVG=SUM;
 
-%% Discretize VVL1
+%% Discretize MAF
 
 X=discretize(log.RPM,xedges);
 Y=discretize(log.MAP,yedges);
 log.X=X;
 log.Y=Y;
 
-columns= zeros(12,8)
-rows=columns
+%% Initialize matrixes
 
-for i=1:length(mafxaxis)
-        temp=log;
-        temp(temp.X~=i,:)=[];
-        columns(:,i) = lsq_lut_piecewise( temp.MAP, temp.ADD_MAF, mafyaxis )
-end
+for IDX=0:3
 
-for j=1:length(mafyaxis)
-        temp=log;
-        temp(temp.Y~=j,:)=[];
-        rows(j,:) = lsq_lut_piecewise( temp.RPM, temp.ADD_MAF, mafxaxis )          
-end
+    SUM=zeros(length(mafyaxis),length(mafxaxis));
+    COUNT=SUM;
+    AVG=SUM;
+    columns= zeros(12,8)
+    rows=columns
+    current=maftables{IDX+1}
+    IDXmodes=find(combmodes==IDX)-1
+    temp1=log
+    diffcmb=setdiff(log.CMB,IDXmodes)
+    for k=1:length(diffcmb)
+        temp1(temp1.CMB==diffcmb(k),:)=[]
+    end
 
-blend=(columns+rows)/2
-
-for i=1:length(mafxaxis)
+    for i=1:length(mafxaxis)
+            temp=temp1;
+            temp(temp.X~=i,:)=[];
+            columns(:,i) = lsq_lut_piecewise( temp.MAP, temp.ADD_MAF, mafyaxis )
+    end
+    
     for j=1:length(mafyaxis)
-        temp=log;
-        temp(temp.X~=i,:)=[];
-        temp(temp.Y~=j,:)=[];
-        AVGtemp=mean(temp.ADD_MAF)
-        COUNT(j,i)=height(temp)
-        if COUNT(j,i)>3
-            ci=paramci(fitdist(temp.ADD_MAF,'Normal'),'Alpha',0.1)
-            sigma(j,i)=ci(2,2)
-            low(j,i)=ci(1,1)
-            high(j,i)=ci(2,1)
-            if low(j,i)>current(j,i)
-                AVG(j,i)=blend(j,i)
-            elseif high(j,i)<current(j,i)
-                AVG(j,i)=blend(j,i)
+            temp=temp1;
+            temp(temp.Y~=j,:)=[];
+            rows(j,:) = lsq_lut_piecewise( temp.RPM, temp.ADD_MAF, mafxaxis )          
+    end
+    
+    blend=(columns+rows)/2
+    interpfac=.25
+    
+    
+    for i=1:length(mafxaxis)
+        for j=1:length(mafyaxis)
+            temp=temp1;
+            temp(temp.X~=i,:)=[];
+            temp(temp.Y~=j,:)=[];
+            AVGtemp=mean(temp.ADD_MAF)
+            COUNT(j,i)=height(temp)
+            if COUNT(j,i)>3
+                ci=paramci(fitdist(temp.ADD_MAF,'Normal'),'Alpha',conf)
+                sigma(j,i)=ci(2,2)
+                low(j,i)=ci(1,1)
+                high(j,i)=ci(2,1)
+                if low(j,i)>current(j,i)
+                    AVG(j,i)=(blend(j,i)*interpfac+low(j,i)*(1-interpfac))/2
+                elseif high(j,i)<current(j,i)
+                    AVG(j,i)=(blend(j,i)*interpfac+high(j,i)*(1-interpfac))/2
+    
+                else
+                    AVG(j,i)=current(j,i)
+                end
             else
                 AVG(j,i)=current(j,i)
             end
-        else
-            AVG(j,i)=current(j,i)
         end
     end
+    
+    COUNT(COUNT>maxcount)=[maxcount]
+    COUNT=COUNT/maxcount
+    
+    COUNT(isnan(COUNT))=0
+    AVG(isnan(AVG))=0
+    CHANGE=(AVG-current).*COUNT
+    NEW=round((current+CHANGE)*5.12,0)/5.12
+    
+    my_field = strcat('IDX',num2str(IDX))
+    results.(my_field)=array2table(NEW,'VariableNames',xlabels,'RowNames',ylabels)
+    fig3 = uifigure('Name',strcat('MAF Results IDX []: ',num2str(IDX)),"Position",[500 500 760 360]);
+    uit = uitable(fig3, "Data",results.(my_field), "Position",[20 20 720 320]);
+    styleIndices = (CHANGE);
+    [row,col] = find(styleIndices);
+    s = uistyle("BackgroundColor",'gr');
+    addStyle(uit,s,"cell",[row,col]);
 end
 
-COUNT(COUNT>maxcount)=[maxcount]
-COUNT=COUNT/maxcount
-
-COUNT(isnan(COUNT))=0
-AVG(isnan(AVG))=0
-CHANGE=(AVG-current).*COUNT*fudge
-NEW=round((current+CHANGE)*5.12,0)/5.12
-
-Res_MAF=array2table(NEW,'VariableNames',xlabels,'RowNames',ylabels)
-fig3 = uifigure('Name','MAF Results',"Position",[500 500 760 360]);
-uit = uitable(fig3, "Data",Res_MAF, "Position",[20 20 720 320]);
-styleIndices = (CHANGE);
-[row,col] = find(styleIndices);
-s = uistyle("BackgroundColor",'gr');
-addStyle(uit,s,"cell",[row,col]);
 end
