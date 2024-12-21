b6263f8fd55e2687db3c067519de3528845160ae
yakboyslim <58483496+yakboyslim@users.noreply.github.com>
2024-12-21 09:31:53 -0500

    Clearing out the main branch for Python implementation

--- BinRead.m
+++
@@ -1,9 +0,0 @@
-function [output] = BinRead(bin,req)
-% [fileopen,pathopen]=uigetfile()
-% bin=fopen(fullfile(pathopen,fileopen));
-
-for j=1:length(req{1})
-fseek(bin,req{1}(j),'bof')
-temp=fread(bin,[req{2}(j) req{3}(j)],req{6}(j))
-output{j}=(temp-req{4}(j))./req{5}(j)
-end
