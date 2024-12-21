2a17c412c1dcc76d46d287a38a6992b838e2951a
yakboyslim <58483496+yakboyslim@users.noreply.github.com>
2024-12-21 09:31:53 -0500

    Clearing out the main branch for Python implementation

--- getcurrentdir.m
+++
@@ -1,7 +0,0 @@
-function currentDir = getcurrentdir
-if isdeployed % Stand-alone mode.
-    [status, result] = system('path');
-    currentDir = char(regexpi(result, 'Path=(.*?);', 'tokens', 'once'));
-else % MATLAB mode.
-    currentDir = pwd;
-end
